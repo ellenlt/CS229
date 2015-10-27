21a60200edccee52f9aad6f897016879dccc6b2c
ellenlt <ellenltsay@gmail.com>
2015-10-27 00:13:59 -0700

    First commit of HW2
    Completed NB, question #3a

---
+++ HW2/nb_test.m
@@ -0,0 +1,97 @@
+nb_train;
+
+[spmatrix, tokenlist, category] = readMatrix('MATRIX.TEST');
+
+testMatrix = full(spmatrix);
+numTestDocs = size(testMatrix, 1);
+numTokens = size(testMatrix, 2);
+
+
+% Assume nb_train.m has just been executed, and all the parameters computed/needed
+% by your classifier are in memory through that execution. You can also assume 
+% that the columns in the test set are arranged in exactly the same way as for the
+% training set (i.e., the j-th column represents the same token in the test data 
+% matrix as in the original training data matrix).
+
+% Write code below to classify each document in the test set (ie, each row
+% in the current document word matrix) as 1 for SPAM and 0 for NON-SPAM.
+
+% Construct the (numTestDocs x 1) vector 'output' such that the i-th entry 
+% of this vector is the predicted class (1/0) for the i-th  email (i-th row 
+% in testMatrix) in the test set.
+output = zeros(numTestDocs, 1);
+
+%---------------
+% YOUR CODE HERE
+
+% logSpamCPs, logNonSpamCPs
+% are (1 x numTokens) vectors containing log conditional probabilities
+% log P(token|spam) and log P(token|non-spam)
+% Computed using LaPlace smoothing.
+
+% logPSpam, logPNonSpam
+% are scalars equal to the log probability that an email is spam or non-spam
+
+% Repeat the rows of logSpamCPs and logNonSpamCPs to generate a
+% (numTestDocs x numTokens) matrix
+logSpamCPsExpanded = repmat(logSpamCPs, numTestDocs, 1);
+logNonSpamCPsExpanded = repmat(logNonSpamCPs, numTestDocs, 1);
+
+% Records positions in testMatrix where a word appears
+wordIdx = testMatrix>0;
+
+% aggregateSpamMatrix and aggregateNonSpamMatrix are (numTestDocs x numTokens) matrices
+% containing log P(token|spam) and log P(token|non-spam) for words which appear
+aggregateSpamMatrix = zeros(numTestDocs, numTokens);
+aggregateSpamMatrix(wordIdx) = logSpamCPsExpanded(wordIdx);
+aggregateNonSpamMatrix = zeros(numTestDocs, numTokens);
+aggregateNonSpamMatrix(wordIdx) = logNonSpamCPsExpanded(wordIdx);
+
+% spam and nonSpam
+% are (numTestDocs x 1) vectors that contain
+% log(P(W|spam)P(spam)) and log(P(W|non-spam)P(non-spam))
+% quantities for each document
+spam = sum(aggregateSpamMatrix,2)+logPSpam;
+nonSpam = sum(aggregateNonSpamMatrix,2)+logPNonSpam;
+
+spamIdx = spam >= nonSpam;
+
+output(spamIdx) = 1;
+
+TESTMATRIX=[1,0;
+			0,1;
+			1,1;
+			5,5;
+			0,3];
+
+NUMTESTDOCS=5;
+NUMTOKENS=2;
+SPAMCONDPROBS=[.5,.6];
+NONSPAMCONDPROBS=[.3,.2];
+
+idx=TESTMATRIX>0;
+
+AGGREGATEPROBSPAM=zeros(NUMTESTDOCS, NUMTOKENS);
+AGGREGATEPROBNOTSPAM=zeros(NUMTESTDOCS, NUMTOKENS);
+SPAMCONDPROBSEXPANDED=repmat(SPAMCONDPROBS, NUMTESTDOCS, 1);
+
+AGGREGATEPROBSPAM(idx)=SPAMCONDPROBSEXPANDED(idx);
+
+sum(AGGREGATEPROBSPAM, 2);
+
+
+%---------------
+
+
+% Compute the error on the test set
+error=0;
+for i=1:numTestDocs
+  if (category(i) ~= output(i))
+    error=error+1;
+  end
+end
+
+%Print out the classification error on the test set
+error/numTestDocs
+
+
---
+++ HW2/nb_train.m
@@ -0,0 +1,51 @@
+#!/usr/local/bin/octave
+
+[spmatrix, tokenlist, trainCategory] = readMatrix('MATRIX.TRAIN');
+trainMatrix = full(spmatrix);
+numTrainDocs = size(trainMatrix, 1);
+numTokens = size(trainMatrix, 2);
+
+% trainMatrix is now a (numTrainDocs x numTokens) matrix.
+% Each row represents a unique document (email).
+% The j-th column of the row $i$ represents the number of times the j-th
+% token appeared in email $i$.
+
+% tokenlist is a long string containing the list of all tokens (words).
+% These tokens are easily known by position in the file TOKENS_LIST
+
+% trainCategory is a (1 x numTrainDocs) vector containing the true 
+% classifications for the documents just read in. The i-th entry gives the 
+% correct class for the i-th email (which corresponds to the i-th row in 
+% the document word matrix).
+
+% Spam documents are indicated as class 1, and non-spam as class 0.
+% Note that for the SVM, you would want to convert these to +1 and -1.
+
+% YOUR CODE HERE
+
+% Find row indices of emails classified as spam or non-spam
+spamIndices = find(trainCategory==1);
+nonSpamIndices = find(trainCategory==0);
+
+% Generate matrices containing spam and non-spam emails
+spamMatrix = trainMatrix(spamIndices,:);
+nonSpamMatrix = trainMatrix(nonSpamIndices,:);
+
+% Vector of token counts for how many times
+% each token appears in spam and non-spam
+spamCounts = sum(spamMatrix);
+nonSpamCounts = sum(nonSpamMatrix);
+
+% logSpamCPs, logNonSpamCPs
+% are (1 x numTokens) Vectors containing log conditional probabilities of seeing token i in an email,
+% given the email is a spam or non-spam. With LaPlace smoothing.
+% Computed with with LaPlace smoothing and using following formula:
+% For spamProbs:
+	% Numerator = # times token i appears in spam + 1
+	% Denominator = # tokens total in spam + # tokens in vocab
+logSpamCPs = log((spamCounts.+1)./(sum(spamCounts)+numTokens));
+logNonSpamCPs = log((nonSpamCounts.+1)./(sum(nonSpamCounts)+numTokens));
+
+% Log probabilities (scalars) that an email is spam or not spam
+logPSpam = log(rows(spamMatrix)/numTrainDocs);
+logPNonSpam = log(rows(nonSpamMatrix)/numTrainDocs);
